f640b67a1f62e2ff6e778c028b5681a4ddddfee1
Jaime <gatomzl@gmail.com>
2017-06-24 14:04:15 -0500

    Fixed for the allowed hosts config

--- myfirstweb/settings.py
+++ myfirstweb/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '(knlpzn(0#=7i%p7ajjwx8%@vhn^^oa#-253&sfg+)r!a3((sm'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
